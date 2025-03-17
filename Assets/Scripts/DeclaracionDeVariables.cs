41cfac5a1cea074fa8490c2c56ea4dbc7da729e6
mercho40 <107892036+mercho40@users.noreply.github.com>
2025-03-17 12:15:38 -0300

    ej 1

---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    string nombre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nombre = "simon";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
